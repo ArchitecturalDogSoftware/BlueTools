d36902dbbc31a4ef372752be7f77d696f1c161ef
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-06-02 12:44:10 -0400

    chore: update formatting

--- src/main/java/net/architecturaldog/bluetools/BlueTools.java
+++ src/main/java/net/architecturaldog/bluetools/BlueTools.java
@@ -3,17 +3,19 @@ package net.architecturaldog.bluetools;
 import dev.jaxydog.lodestone.Lodestone;
 import dev.jaxydog.lodestone.api.CommonLoaded;
 import net.fabricmc.api.ModInitializer;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class BlueTools implements ModInitializer {
-	public static final String MOD_ID = "bluetools";
+public class BlueTools
+    implements ModInitializer
+{
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final String MOD_ID = "bluetools";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
-	public void onInitialize() {
-            Lodestone.load(CommonLoaded.class, MOD_ID);
-	}
+    @Override
+    public void onInitialize() {
+        Lodestone.load(CommonLoaded.class, MOD_ID);
+    }
+
 }
