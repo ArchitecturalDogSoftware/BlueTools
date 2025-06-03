bba6a44e161a90912a77f000938fd4ac72cc6784
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-06-03 13:48:25 -0400

    fix: add materials class to root content loader

--- src/main/java/net/architecturaldog/bluetools/content/BlueToolsContent.java
+++ src/main/java/net/architecturaldog/bluetools/content/BlueToolsContent.java
@@ -8,6 +8,7 @@ import net.architecturaldog.bluetools.content.block.BlueToolsBlockEntityTypes;
 import net.architecturaldog.bluetools.content.block.BlueToolsBlocks;
 import net.architecturaldog.bluetools.content.data.BlueToolsResourceReloadListeners;
 import net.architecturaldog.bluetools.content.item.BlueToolsItems;
+import net.architecturaldog.bluetools.content.material.BlueToolsMaterials;
 import net.minecraft.util.Identifier;
 
 public final class BlueToolsContent extends AutoLoader {
@@ -18,6 +19,7 @@ public final class BlueToolsContent extends AutoLoader {
     public static final BlueToolsBlocks BLOCKS = new BlueToolsBlocks();
     public static final BlueToolsBlockEntityTypes BLOCK_ENTITY_TYPES = new BlueToolsBlockEntityTypes();
     public static final BlueToolsItems ITEMS = new BlueToolsItems();
+    public static final BlueToolsMaterials MATERIALS = new BlueToolsMaterials();
     @LoadingPriority(Integer.MAX_VALUE)
     public static final BlueToolsRegistries REGISTRIES = new BlueToolsRegistries();
     public static final BlueToolsResourceReloadListeners RESOURCE_RELOAD_LISTENERS =
