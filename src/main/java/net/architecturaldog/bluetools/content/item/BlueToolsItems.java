6b863a741d0090c02b1e0571941aaacc6376fee2
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2025-06-04 13:13:40 -0400

    refactor: remove custom item classes in favor of registry wrapper

--- src/main/java/net/architecturaldog/bluetools/content/item/BlueToolsItems.java
+++ src/main/java/net/architecturaldog/bluetools/content/item/BlueToolsItems.java
@@ -2,9 +2,16 @@ package net.architecturaldog.bluetools.content.item;
 
 import dev.jaxydog.lodestone.api.AutoLoader;
 import net.architecturaldog.bluetools.BlueTools;
+import net.architecturaldog.bluetools.utility.RegistryLoaded;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
 public final class BlueToolsItems extends AutoLoader {
+
+    private static <T extends Item> RegistryLoaded<T> create(final String path, final T value) {
+        return new RegistryLoaded<>(path, Registries.ITEM, value);
+    }
 
     @Override
     public Identifier getLoaderId() {
