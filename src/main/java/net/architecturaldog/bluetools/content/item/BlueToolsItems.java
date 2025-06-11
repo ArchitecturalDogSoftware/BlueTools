c16da581fc1500000d39ccef3cbad0b83d833f87
IcePenguin <130691092+IcePenguin1@users.noreply.github.com>
2025-06-11 00:26:20 -0400

    feat: added forge interface block item

--- src/main/java/net/architecturaldog/bluetools/content/item/BlueToolsItems.java
+++ src/main/java/net/architecturaldog/bluetools/content/item/BlueToolsItems.java
@@ -2,7 +2,9 @@ package net.architecturaldog.bluetools.content.item;
 
 import dev.jaxydog.lodestone.api.AutoLoader;
 import net.architecturaldog.bluetools.BlueTools;
+import net.architecturaldog.bluetools.content.block.BlueToolsBlocks;
 import net.architecturaldog.bluetools.utility.RegistryLoaded;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
@@ -12,6 +14,11 @@ import net.minecraft.util.Identifier;
 import java.util.function.Function;
 
 public final class BlueToolsItems extends AutoLoader {
+
+    public static final RegistryLoaded<BlockItem> FORGE_INTERFACE = create(
+        "forge_interface",
+        settings -> new BlockItem(BlueToolsBlocks.FORGE_INTERFACE, settings.useBlockPrefixedTranslationKey())
+    );
 
     private static <T extends Item> RegistryLoaded<T> create(final String path, Function<Item.Settings, T> function) {
         final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, BlueTools.id(path));
